77df992a0d109979d7dd1edc57a2cc0481121843
Mudoker <huuquoc76030@gmail.com>
2022-10-31 12:10:23 +0700

    Web connect is now automatic

--- source_code.py
+++ source_code.py
@@ -19,57 +19,57 @@ client = discord.Client(intents=discord.Intents.all())
 # await -> prior to finish this
 @client.event
 async def on_ready():
-  print("{0.user} - created by group of friends".format(client))
+    print("{0.user} - created by group of friends".format(client))
 
 
 @client.event
 async def on_voice_state_update(member, before, after):
-  user_id = str(member.id)
+    user_id = str(member.id)
 
-  # If user enter voice room
-  if before.channel is None and after.channel is not None:
-    join_time = round(time.time())
+    # If user enter voice room
+    if before.channel is None and after.channel is not None:
+        join_time = round(time.time())
 
-    # if user already in database -> update join time
-    if user_id in db.keys():
-      db[user_id]["join_time"] = join_time
-    # create a new user if not find
+        # if user already in database -> update join time
+        if user_id in db.keys():
+            db[user_id]["join_time"] = join_time
+        # create a new user if not find
+        else:
+            db[user_id] = {"join_time": join_time, "total_study_time": 0}
     else:
-      db[user_id] = {"join_time": join_time, "total_study_time": 0}
-  else:
-    leave_time = round(time.time())
-    study_time = leave_time - \
-        db[user_id]["join_time"] + db[user_id]["total_study_time"]
-    db[user_id]["total_study_time"] = study_time
+        leave_time = round(time.time())
+        study_time = leave_time - \
+                     db[user_id]["join_time"] + db[user_id]["total_study_time"]
+        db[user_id]["total_study_time"] = study_time
 
 
 @client.event
 async def on_message(message):
-  # Check if the message is sent by the bot
-  if message.author == client.user:
-    return
+    # Check if the message is sent by the bot
+    if message.author == client.user:
+        return
 
-  # Command symbol
-  if message.content.startswith('$hello'):
-    await message.channel.send('Lo cc')
+    # Command symbol
+    if message.content.startswith('$hello'):
+        await message.channel.send('Lo cc')
 
-  # Load data
-  if message.content.startswith('$study'):
-    user_id = str(message.author.id)
-    if user_id in db.keys(): 
-      study_time = db[user_id]["total_study_time"]
-      await message.channel.send(
-        f"<@{user_id}>'s study time is: {study_time} seconds")
-    else:
-      await message.channel.send('No data found!')
+    # Load data
+    if message.content.startswith('$study'):
+        user_id = str(message.author.id)
+        if user_id in db.keys():
+            study_time = db[user_id]["total_study_time"]
+            await message.channel.send(
+                f"<@{user_id}>'s study time is: {study_time} seconds")
+        else:
+            await message.channel.send('No data found!')
 
-  #Reset data
-  if message.content.startswith('$rreset'):
-    user_id = str(message.author.id)
-    await message.channel.send(f"Deleting <@{user_id}>'s study reccord")
-    if user_id in db.keys():
-      del db[user_id]
-    await message.channel.send('Deleted!')
+    #Reset data
+    if message.content.startswith('$rreset'):
+        user_id = str(message.author.id)
+        await message.channel.send(f"Deleting <@{user_id}>'s study reccord")
+        if user_id in db.keys():
+            del db[user_id]
+        await message.channel.send('Deleted!')
 
 # Connect with web server
 keep_alive()
